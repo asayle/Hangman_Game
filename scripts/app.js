14260491a57b40225748355395d88abd863ab11a
Amelia Sayle <amelia@co-asayle1910.local>
2020-08-10 16:50:05 -0600

    dfhdfh

---
+++ scripts/app.js
@@ -0,0 +1,63 @@
+const puzzleEl = document.querySelector('#puzzle')
+const guessesEl = document.querySelector('#guesses')
+let game1 
+
+window.addEventListener('keypress', (e) => {
+    const guess = String.fromCharCode(e.charCode)
+    game1.makeGuess(guess)
+    render()
+})
+
+const render = () => {
+    puzzleEl.innerHTML = ''
+    guessesEl.textContent = game1.statusMessage
+
+    game1.puzzle.split('').forEach((letter) => {
+        const letterEl = document.createElement('span')
+        letterEl.textContent = letter
+        puzzleEl.appendChild(letterEl)
+    })
+}
+
+const startGame = async () => {
+    const puzzle = await getPuzzle('2')
+    game1 = new Hangman(puzzle, 5)
+    render()
+}
+
+document.querySelector('#reset').addEventListener('click', startGame)
+
+startGame()
+
+// getPuzzle('2').then((puzzle) => {
+//     console.log(puzzle)
+// }).catch((err) => {
+//     console.log(`error: ${err}`)
+// })
+
+// getCurrentCountry().then((country) => {
+//     console.log(country.name)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+// getLocation().then((data) => {
+//     return getCountry((data.country))
+// }).then((country) => {
+//     console.log(country.name)
+// }).catch((err) => {
+//     console.log(`error: ${err}`)
+// })
+
+// fetch('http://puzzle.mead.io/puzzle', {}).then((response) =>{ 
+//     if (response.status === 200){
+//         return response.json()
+//     }else {
+//         throw new Error('unable to fetch puzzle')
+//     }
+// }).then((data) => {
+//     console.log(data.puzzle)
+// }).catch((error) =>{
+//     console.log(error)
+// })
+
---
+++ scripts/hangman.js
@@ -0,0 +1,78 @@
+
+// create a constructor function for the hangman game
+// set up two attributes. One for the word itself, another for the number of guesses allowed.
+// create two instances of it and print both to the console.
+
+// set up the word instance property as an array of lowercase letters
+// set up another instance property to store guessed letters
+// create a method that gives you the word puzzle back
+
+// Create a method for making a guess
+// 1.should accept a character for guessing
+// 2.should add unique guesses to list of guesses
+// 3.should decrement the guesses left if a unique guess isnt a match
+
+// display the puzzle to the browser instead of console
+// display the guesses left to the browser instead of console
+// separate the hangman definition from the rest of the app code (use app.js)
+
+class Hangman {
+    constructor(word, remainingGuesses) {
+        this.word = word.toLowerCase().split('')
+        this.remainingGuesses = remainingGuesses
+        this.guessedLetters = []
+        this.status = 'playing'
+    }
+    calculateStatus() {
+        const finished = this.word.every((letter) => this.guessedLetters.includes(letter) || letter === ' ')
+
+        if (this.remainingGuesses === 0) {
+            this.status = 'failed'
+        } else if (finished) {
+            this.status = 'finished'
+        } else {
+            this.status = 'playing'
+        }
+    }
+    get statusMessage() {
+        if (this.status === 'playing') {
+            return `Guesses left: ${this.remainingGuesses}`
+        } else if (this.status === 'failed') {
+            return `Nice try! The word was "${this.word.join('')}".`
+        } else {
+            return 'Great work! You guessed the word.'
+        }
+    }
+    get puzzle() {
+        let puzzle = ''
+
+        this.word.forEach((letter) => {
+            if (this.guessedLetters.includes(letter) || letter === ' ') {
+                puzzle += letter
+            } else {
+                puzzle += '*'
+            }
+        })
+
+        return puzzle
+    }
+    makeGuess(guess) {
+        guess = guess.toLowerCase()
+        const isUnique = !this.guessedLetters.includes(guess)
+        const isBadGuess = !this.word.includes(guess)
+
+        if (this.status !== 'playing') {
+            return
+        }
+
+        if (isUnique) {
+            this.guessedLetters.push(guess)
+        }
+
+        if (isUnique && isBadGuess) {
+            this.remainingGuesses--
+        }
+
+        this.calculateStatus()
+    }
+}
